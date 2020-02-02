d6b20f60a9224a1f0772b4b8fbd11522a46bf624
nake89 <nake89@gmail.com>
2020-02-02 22:08:59 +0200

    #10 Added more tests

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -5,7 +5,7 @@ require("dotenv").config({path: process.cwd() + "/.env.testing"})
 describe("user tests", () => {
   beforeAll(() => {})
 
-  it("saveUser test", async () => {
+  it("saveUser_success", async () => {
     const uc = new userController()
     const saveUserResult = await uc.saveUser(
       "nake89@gmail.com",
@@ -16,5 +16,33 @@ describe("user tests", () => {
     expect(saveUserResult.data.userId).toBe(1)
     expect(saveUserResult.data.username).toBe("nake89@gmail.com")
     expect(saveUserResult.data.message).toBe("User successfully created!")
+  })
+
+  it("saveUser_user_already_exists", async () => {
+    const uc = new userController()
+    const saveUserResult = await uc.saveUser(
+      "nake89@gmail.com",
+      "nake89@gmail.com",
+      "asdaAa12aaaa3!!"
+    )
+    expect(saveUserResult.ok).toBe(0)
+    expect(saveUserResult.data.username).toBe("nake89@gmail.com")
+    expect(saveUserResult.data.message).toBe(
+      "Couldn't create user. User exists"
+    )
+  })
+
+  it("saveAndloginUser_test", async () => {
+    const uc = new userController()
+    const username = "nake89+new@gmail.com"
+    const password = "asdaAa12aaaa3!!"
+    const saveUserResult = await uc.saveUser(username, username, password)
+    const loginUserResult = await uc.loginUser(username, password)
+    console.log(loginUserResult)
+    expect(saveUserResult.ok).toBe(1)
+    expect(saveUserResult.data.userId).toBe(2)
+    expect(saveUserResult.data.username).toBe(username)
+    expect(saveUserResult.data.message).toBe("User successfully created!")
+    expect(loginUserResult.ok).toBe(1)
   })
 })
