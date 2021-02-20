f9cebe98786aff1b130cef93876bcc90bb0a235d
Kevin Kivi <nake89@gmail.com>
2021-02-21 01:38:58 +0200

    Fix/integration test (#142)
    * fix: added integration tests
    
    I have to use fix here instead of ci, otherwise test wont run
    
    * fix: ci now running spawn sls offline

--- packages/kuka-serverless/scripts/ci.sh
+++ packages/kuka-serverless/scripts/ci.sh
@@ -4,5 +4,6 @@ npm config set @kuka-js:registry http://registry.npmjs.org
 npm config set //registry.npmjs.org/:_authToken $NPM_TOKEN
 npm whoami
 npm ci --also=dev
+node sls-offline-test-runner.js
 npm test
 npm run semantic-release
