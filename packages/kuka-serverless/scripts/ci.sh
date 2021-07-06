83a09683db3d10be11fee9b83dd3e85abf0cb671
nake89 <nake89@gmail.com>
2021-07-06 11:57:57 +0300

    debugging ci issues

--- packages/kuka-serverless/scripts/ci.sh
+++ packages/kuka-serverless/scripts/ci.sh
@@ -5,5 +5,6 @@ npm config set //registry.npmjs.org/:_authToken $NPM_TOKEN
 npm whoami
 npm ci --also=dev
 node spawn-sls-offline.js
+curl --verbose -X POST -H 'Content-Type: application/json' -d '{"username":"nake89+debug1@gmail.com","email":"nake89+debug1@gmail.com","password":"nake89@gmail.COM"}' http://localhost:4000/test/register
 npm test
 npm run semantic-release
