9d4bacf84c7730bba32dca0d169aca38f9bbab1b
Kevin Kivi <nake89@gmail.com>
2021-12-14 10:41:46 +0200

    fix: npm notice error. Switch from http to https (#179)

--- packages/kuka-core/scripts/ci.sh
+++ packages/kuka-core/scripts/ci.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-npm config set @kuka-js:registry http://registry.npmjs.org
+npm config set @kuka-js:registry https://registry.npmjs.org
 npm config set //registry.npmjs.org/:_authToken $NPM_TOKEN
 npm whoami
 npm ci --also=dev
